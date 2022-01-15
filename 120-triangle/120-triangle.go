0b7fb91d4c25a434e185d528a01941edc62e2fc6
Anindya Biswas <anindya33046@gmail.com>
2022-01-15 19:37:08 +0600

    Time: 5 ms (36.51%), Space: 3.6 MB (45.64%) - LeetHub

---
+++ 120-triangle/120-triangle.go
@@ -0,0 +1,20 @@
+func minimumTotal(triangle [][]int) int {
+    n:=len(triangle)
+    //levelminimum:=make([]int,level)
+    
+    //levelminimum[0] = triangle[0][0]
+
+    for i :=n-2;i>=0;i--{
+        for j:=i;j>=0;j--{
+            triangle [i][j] += min(triangle [i+1][j],triangle [i+1][j+1]) 
+        }
+    }
+    return triangle[0][0]
+}
+func min(a int, b int ) int{
+    if a > b{
+        return b
+    }else{
+        return a
+    }
+}
