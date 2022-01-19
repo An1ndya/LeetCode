5e1e5cb54ea8f2d270b0d0f083a948dd4c2dc4d6
Anindya Biswas <anindya33046@gmail.com>
2022-01-19 11:54:20 +0600

    Time: 0 ms (100.00%), Space: 2.5 MB (57.51%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,29 @@
+func findMin(nums []int) int {
+    min:= math.MaxInt64
+    n:=len(nums)
+    l:=0 
+    r:=n-1
+    
+    for l <= r {
+        
+        //fmt.Println(l, " ",r)
+        
+        m:= int((l+r)/2)
+        
+        if nums[m] < min { min =nums[m]}
+        
+        if nums[l] < nums[m] {
+            
+            if nums[l] < min { min =nums[l]}
+            
+ 
+                l = m + 1
+            
+        }else{
+            if nums[r] < min { min = nums[r]}
+
+            r = m - 1
+        }
+    }
+    return min
+}
