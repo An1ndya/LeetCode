89a4567f291fa0adb7f3162d369b9df9dd53a6f0
Anindya Biswas <anindya33046@gmail.com>
2022-01-30 07:09:46 +0600

    Time: 218 ms (48.37%), Space: 15 MB (89.42%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[-1]*n
-        dp[n-1] = 0
-        for i in range(n-2,-1,-1):
-            mx=i+nums[i]
-            if mx > n-1:
-                dp[i]=1
-            else:
-                for j in range(mx,i,-1):
-                    if dp[j] !=-1:
-                        mn=dp[j]+1
-                        if dp[i]==-1 or mn<dp[i]:
-                            dp[i]=mn
-                    if dp[i]!=-1 and dp[i]==2:
-                        break   
-        return dp[0]       
+        if n==1 : return 0
+        step=0
+        curstepend=0
+        mx=0
+        for i in range(0,n,1):
+            
+            mx=max(mx,i+nums[i])
+            
+            if curstepend==i: 
+                step+=1
+                curstepend=mx
+                if curstepend>=n-1:
+                    break
+        return step     
