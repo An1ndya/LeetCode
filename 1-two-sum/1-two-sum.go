2b9d151a37922154a5ee3696ee80e602e25f5752
Anindya Biswas <anindya33046@gmail.com>
2022-02-08 01:48:31 +0600

    Time: 13 ms (49.36%), Space: 4.3 MB (41.38%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -2,22 +2,11 @@ func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     n := len(nums)
     for i:=0;i<n;i++ {
-        
-        if value, found := m[nums[i]]; found {
-            //same element found 
-            //check if match target
-            if 2*nums[i] == target {
-                return []int{value,i}
-            }
-        }else{
-            complement := target - nums[i]
-            
-            if value, found := m[complement]; found{
-                return []int{value,i}
-            }    
-            
-            m[nums[i]] = i
-        }
+
+        if value, found := m[target - nums[i]]; found{
+            return []int{value,i}
+        }    
+        m[nums[i]] = i 
     }
     return []int{0,0}
 }
