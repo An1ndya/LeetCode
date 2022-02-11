0122282186050f5f72d9850a29396a81340d0b92
Anindya Biswas <anindya33046@gmail.com>
2022-02-11 16:00:12 +0600

    Time: 7 ms (74.09%), Space: 43 MB (27.09%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,24 +1,29 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
+    public boolean checkInclusion(String s1, String s2) 
+    {
+                
+        int n1=s1.length(), n2=s2.length(), index=0;
+        
+        if(n1>n2) return false;
+        
         int charfrequency1[]= new int[26];
         Arrays.fill(charfrequency1, 0);
-        int n1=s1.length(),n2=s2.length(),index=0;
-        if(n1>n2) return false;
-        for(int i=0;i<n1;i++) charfrequency1[s1.charAt(i)-'a']++;
+        
         int charfrequency2[]= new int[26];
         Arrays.fill(charfrequency2, 0);
-        for(int i=0;i<n2;i++) 
+        
+        for(int i=0;i<n1;i++)  charfrequency1[s1.charAt(i)-'a']++;
+
+        for(int i=0;i<n1;i++)  charfrequency2[s2.charAt(i)-'a']++;
+        //check for first n1 length substring
+        if(Arrays.equals(charfrequency1, charfrequency2)) return true;
+    
+        for(int i=n1;i<n2;i++) 
         {
             charfrequency2[s2.charAt(i)-'a']++;
-            if(i==n1-1)
-            {
-               if(Arrays.equals(charfrequency1, charfrequency2)) return true;
-            }
-            else if(i>=n1)
-            {
-                charfrequency2[s2.charAt(i-n1)-'a']--;
-                if(Arrays.equals(charfrequency1, charfrequency2)) return true;
-            }
+            charfrequency2[s2.charAt(i-n1)-'a']--;
+            //check for substring between n1 to n2-1 index substring
+            if(Arrays.equals(charfrequency1, charfrequency2)) return true;  
         }
         return false;
     }
