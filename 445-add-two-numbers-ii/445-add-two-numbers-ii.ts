bbd196774a8735c89407c8c98ed3f248a48b429a
Anindya Biswas <anindya33046@gmail.com>
2022-04-04 15:16:15 +0600

    Time: 169 ms (29.63%), Space: 48.2 MB (92.59%) - LeetHub

---
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.ts
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    l1=reverseList(l1);
+    l2=reverseList(l2);
+    let add = addTwoReverseNumber(l1,l2);
+    return reverseList(add);;
+};
+function reverseList(head: ListNode): ListNode |null
+{
+    let prev= null;
+    let next=null;
+    let cur=head;
+
+    while( cur!=null)
+    {
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    head = prev;
+    return head;
+}
+function addTwoReverseNumber(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+        let  head=null;
+        let  prev=null;
+
+        let  add=0;
+        while(l1!=null || l2!=null || add!=0)
+        {
+            let n=0;
+            if(l1!=null && l2!=null)
+            {
+                n=l1.val+l2.val;
+                l1=l1.next;
+                l2=l2.next;
+            }
+            else if(l1!=null)
+            {
+                n=l1.val;
+                l1=l1.next;
+            }
+            else if(l2!=null)
+            {
+                n=l2.val;
+                l2=l2.next;
+            }
+            
+            n+=add;
+            //console.log(n);
+            if(n>9)
+            {
+                add=float2int(n/10);
+                n=n%10;
+            }else{
+                add=0;
+            }
+            let node = new ListNode(n);
+            if(head==null)
+            {
+                head = node;           
+            }
+            else
+            {
+                prev.next= node;
+            }
+            prev = node; 
+        }
+        return head;
+};
+function float2int (value) {
+    return value | 0;
+}
