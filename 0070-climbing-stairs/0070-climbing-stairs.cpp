a94ad3570916a72a57f1208b2b5508965f35b4b4
Anindya Biswas <anindya33046@gmail.com>
2024-01-18 17:34:32 +0600

    Time: 2 ms (45.07%), Space: 6.3 MB (71.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        int ara[n+2];
+        ara[1] = 1;        //1 step
+        ara[2] = 2;        //1+1,2 : 2 combination 
+        for(int i=3;i<=n;i++)
+        {
+            //come to point i by climbing 1 step from i-1
+            //or by climbing 2 step from i-2
+            //so it can reach by sum total of i-1 and i-2's combination 
+            ara[i] = ara[i-1] + ara[i-2];
+        }
+        
+        return ara[n];
+    }
+};
