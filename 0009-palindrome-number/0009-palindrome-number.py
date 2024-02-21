03ef9ec190c70c46745138d70174956308b4381e
Anindya Biswas <anindya33046@gmail.com>
2024-02-21 19:11:14 +0600

    Time: 46 ms (88.19%), Space: 16.7 MB (55.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s = str(x)
-        n = len(s)
+        if x < 0 :
+            return False
+        arr = []
+        while x > 0:
+            #take the last digit 
+            remainder = x % 10
+            arr.append(remainder)
+            #get next number excluding last digit 
+            x = x // 10
+
+        n = len(arr)
+        
         for i in range(n//2):
-            if s[i]!=s[n-1-i]: 
-                return False
+            #check with mirror point 
+            #if dont match then not pallindrome
+            if arr[i] != arr[n-1-i]:
+                return False 
+        #loop passed, so a palindrome  
         return True
