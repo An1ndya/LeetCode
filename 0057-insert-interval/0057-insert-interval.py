28ca329eb13b189b24a3a16415ab9e9f752ed8d5
Anindya Biswas <anindya33046@gmail.com>
2024-03-17 13:53:06 +0600

    Time: 83 ms (15.93%), Space: 19.9 MB (39.60%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -2,6 +2,7 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         n = len(intervals)
         i=0
+        #first add newInterval according to start position
         while i<=n:
             #assign new interval  at sorted order
             if  i==n:
@@ -14,18 +15,18 @@ class Solution:
                 break
             else:
                 i+=1
-        #print(intervals)
-        #print(i)
         # merge overlap
         i=1
         while i<n:
-            #check for overlapp
+            #check for overlapp 
+            #if previous end overlap current start
             if intervals[i-1][1]>=intervals[i][0]:
                 #merged interval to prior element
                 intervals[i-1][1] = max(intervals[i-1][1], intervals[i][1])
                 intervals.pop(i)
                 n-=1
+                #i dont need to be increase to check next 
             else:
                 i+=1
-            #print(i)
+                #check next 
         return intervals
