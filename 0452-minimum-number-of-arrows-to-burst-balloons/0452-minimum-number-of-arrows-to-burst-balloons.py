7b28713c24e800743d0b1fb0cf528ecbe5a604d6
Anindya Biswas <anindya33046@gmail.com>
2024-03-18 17:11:20 +0600

    Time: 1082 ms (44.10%), Space: 62.7 MB (64.62%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,14 +1,25 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         n=len(points)
-        c=1
+        arrows=1
         points = sorted(points,key=lambda x: (x[0],x[1]))
-        #print(points)
-        x=points[0][1]
+        #pass 1st arrow with last x psotion of ballon
+        # cause Xend might hit other overlapping ballon as they are sorted
+        arrowX=points[0][1]
+        
         for i in range(1,n):
-            if x<points[i][0]:
-                c+=1
-                x=points[i][1]
+            if arrowX < points[i][0]:
+                #current ballon is not overlapped with previous ballon
+                #so we need to throw a new arrow
+                arrows += 1
+                #last x position of ballon as our arrowX
+                #same reason, so we may burst overlapping ballon lies front
+                arrowX = points[i][1]
             else:
-                x=min(x,points[i][1])
-        return c
+                #the ballon overlapped 
+                #so we will adjust arrow x such that 
+                # it pass one of ballon's end position which is minimum 
+                #minimum cause to misplace the arrow for next overlapping condition 
+                #means arrow covers the first ballon always 
+                arrowX = min(arrowX,points[i][1])
+        return arrows
