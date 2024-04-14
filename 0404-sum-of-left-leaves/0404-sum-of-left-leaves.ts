bb73ebc556c2920b83fa6a5a603fe86ce6b9c0a9
Anindya Biswas <anindya33046@gmail.com>
2024-04-14 16:17:38 +0600

    Time: 70 ms (18.80%), Space: 52.3 MB (36.84%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sumOfLeftLeaves(root: TreeNode | null): number {
+    
+    if(TreeNode==null) return 0;
+    
+    return DFS(root, false);
+};
+function DFS(root: TreeNode , isleft: boolean): number 
+{
+    // isLeft : if it is a left child, as we need to consider only them
+    if(root==null) return 0;
+    if(root.left==null && root.right==null && isleft) return root.val;
+    return DFS(root.left, true) + DFS(root.right, false);
+};
